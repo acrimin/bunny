c633097795a46e5f2f658e82e7fee6d935b45c4c
Andrew Nicholas Criminski <acrimin@titan5.cs.clemson.edu>
2017-03-01 02:55:23 -0500

    set some colors, started adding user interface

--- bunny.c
+++ bunny.c
@@ -29,8 +29,8 @@ void set_light() {
     float light0_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
     float light0_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
     float light0_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    float light0_position[] = { 0.22, 0.42, 0.6, 1.0 };
-    float light0_direction[] = { -0.22, -0.22, -0.5, 1.0};
+    float light0_position[] = { 0.22, 0.55, 0.6, 1.0 };
+    float light0_direction[] = { -0.22, -0.35, -0.5, 1.0};
 
     // Turn off scene default ambient.
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT,light0_ambient);
@@ -50,22 +50,24 @@ void set_light() {
     glLightfv(GL_LIGHT0,GL_SPOT_DIRECTION,light0_direction);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+
 } 
 
 void set_material() {
-    float mat_diffuse[] = {0.4,0.3,0.0,1.0};
-    float mat_specular[] = {0.9,0.1,0.0,1.0};
-    float mat_shininess[] = {10.0};
+    float mat_diffuse[] = {0.5,0.4,0.0,1.0};
+    float mat_specular[] = {0.7,0.5,0.0,1.0};
+    float mat_shininess[] = {20.0};
 
     glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
     glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
     glMaterialfv(GL_FRONT,GL_SHININESS,mat_shininess);
 }
 
+float eye[] = {0.05,0.5,1.0};
+float viewpt[] = {-0.02,0.1,0.0};
+float up[] = {0.0,1.0,0.0};
+
 void view_volume() {
-    float eye[] = {0.05,0.5,1.0};
-    float viewpt[] = {-0.02,0.1,0.0};
-    float up[] = {0.0,1.0,0.0};
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -144,6 +146,12 @@ void draw_bunny() {
 void renderScene(void) {
     glClearColor(0.3,0.3,0.3,1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    gluLookAt(eye[0],eye[1],eye[2],
+        viewpt[0],viewpt[1],viewpt[2],
+        up[0],up[1],up[2]);
+
     draw_bunny();
     glutSwapBuffers();
 }
@@ -174,13 +182,39 @@ unsigned int set_shaders() {
     return(p);
 }
 
-void getout(unsigned char key, int x, int y) {
+void controller(unsigned char key, int x, int y) {
+    // angle of rotation for the camera direction
+    float scale=0.2;
+    float look[] = {eye[0] - viewpt[0],
+                    eye[1] - viewpt[1],
+                    eye[2] - viewpt[2]
+    };
+    float dis = sqrt(look[0]*look[0]+
+                     look[1]*look[1]+
+                     look[2]*look[2]);
+    float norm_view[] = {look[0]/dis, look[1]/dis, look[2]/dis};
+    printf("%f %f %f\n", norm_view[0], norm_view[1], norm_view[2]);
     switch(key) {
         case 'q':
             exit(1);
+        case 'w':
+            eye[0] -= norm_view[0] * scale;
+            eye[1] -= norm_view[1] * scale;
+            eye[2] -= norm_view[2] * scale;
+            break;
+        case 's':
+            eye[0] += norm_view[0] * scale;
+            eye[1] += norm_view[1] * scale;
+            eye[2] += norm_view[2] * scale;
+            break;
+        case 'a':
+            break;
+        case 'd':
+            break;
         default:
             break;
     }
+    renderScene();
 }
 
 int main(int argc, char **argv) {
@@ -196,7 +230,7 @@ int main(int argc, char **argv) {
     set_material();
     set_shaders();
     glutDisplayFunc(renderScene);
-    glutKeyboardFunc(getout);
+    glutKeyboardFunc(controller);
     glutMainLoop();
     return 0;
 }
